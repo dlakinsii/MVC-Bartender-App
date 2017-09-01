0d467d1fa7591aadc56b0664c297078a71ec52fc
dlakinsii <31526420+dlakinsii@users.noreply.github.com>
2017-08-31 22:42:01 -0400

    Add files via upload

---
+++ Cocktail.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Bartender_App.Models
+{
+    public class Cocktail
+    {
+        
+        public int CocktailId { get; set; }
+        public string CocktailName { get; set; }
+        public string FirstIngredient { get; set; }
+        public string SecondIngredient { get; set; }
+        public string ThirdIngredient { get; set; }
+        public string FourthIngredient { get; set; }
+        public string FifthIngredient { get; set; }
+}
+    
+
+}
---
+++ CocktailViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_Bartender_App.Models;
+
+namespace MVC_Bartender_App.ViewModels
+{
+    public class CocktailViewModel : Controller
+    {
+       public List<Cocktail> Cocktails { get; set; }
+        public Customer ACustomer { get; set; } 
+        public Bartender ABartender { get; set; }
+
+    }
+}
